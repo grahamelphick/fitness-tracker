9f3f5dc33418dd15ddc14fe5887c18aee9fd54f6
Gragit <gme2468@gmail.com>
2021-01-07 14:55:23 +0800

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -17,15 +17,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.use(require("./routes/api-routes.js"));
-// app.use(require("./routes/html-routes.js"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
